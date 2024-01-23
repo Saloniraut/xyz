0480af3b8a70d16c8f335104fd7f2cea93e96a69
Saloniraut <137715285+Saloniraut@users.noreply.github.com>
2024-01-23 06:02:21 +0000

    Pending changes exported from your codespace

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import app
+def test_hello_world():
+    response = app.app.test_client().get("/hello")
+    assert response.status_code == 200
+    assert response.data == b'Hello'
+    
+def test_goodbye():
+    response = app.app.test_client().get("/goodbye")
+    assert response.status_code == 404
